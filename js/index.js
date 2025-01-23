7aaafb4edab4382ee0584064385808d1e7c4b03f
anar123123 <arashidzade1991@gmail.com>
2025-01-24 02:22:02 +0400

    chng-prj

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
 const url = "https://dummyjson.com/products";
-const container = document.querySelector(".container")
+const container = document.querySelector(".standart-container");
+const newproduct = document.querySelector(".new-product");
 
-fetch(url)
-.then((resp)=>resp.json())
-.then((data)=>{
-    const {products} = data;
-    products.filter(item =>{
-        const { thumbnail, price, discountPercentage, description } = item;
-        const oldprice = (price + (price * discountPercentage) / 100).tofixed(2)
-        let html = `
+
+function addtext(item , box){
+  const { thumbnail, price, discountPercentage, description } = item;
+      const oldprice = price + (price * discountPercentage) / 100;
+      let html = `
             <div class="item">
           <div class="img-box">
             <img src="${thumbnail}" alt="" />
@@ -16,12 +14,12 @@ fetch(url)
           <div class="data-box">
             <div class="price-box">
               <p class="current-price">${price}<i class="fa-solid fa-euro-sign"></i></p>
-              <p class="old-price">${oldprice}</p>
+              <p class="old-price">${oldprice.toFixed(2)}</p>
             </div>
             <p class="month-price">12 | 60</p>
             <p class="desc">${description}</p>
           </div>
-          <div class="btn">
+          <div class="actions">
             <button class="icon-heart">
               <i class="fa-solid fa-heart"></i>
             </button>
@@ -32,6 +30,26 @@ fetch(url)
           </div>
         </div>
         </div>`;
-        container.insertAdjacentHTML("beforeend", html)
-    })
-})
+        // return html 
+        box.insertAdjacentHTML("beforeend", html);
+
+}
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => {
+    const { products } = data;
+    const productsData = products.filter((item) => item.id <= 8);
+    productsData.forEach((item) => {
+      // const result = addtext(item)
+      // container.insertAdjacentHTML("beforeend", result);
+      addtext(item,container )
+    });
+    productsData
+      .filter((item) => item.id <= 4)
+      .forEach((item) => {
+        // const result = addtext(item)
+        // newproduct.insertAdjacentHTML("beforeend", result);
+        addtext(item, newproduct)
+      });
+  });
